f1eff2fb85eb2e4be421c77cbfe0277047309507
Ben Slater <ben@overmation.com>
2015-03-23 16:23:05 +0000

    changed pinteresting into Tap and Filter

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about"
-  get "contact" => "pages#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
